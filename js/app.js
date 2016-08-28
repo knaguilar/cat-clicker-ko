a5dcd00cb02ee0a841d6a49dd3ba3b7dcd240415
Karla Aguilar <kn.aguilar7@gmail.com>
2016-08-27 23:00:58 -0400

    feat: add levels to cats

--- js/app.js
+++ js/app.js
@@ -7,6 +7,29 @@ var ViewModel = function() {
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
+	this.levels = ko.computed(function() {
+		var levels;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			levels = 'Newborn';
+		}
+		else if (clicks < 50) {
+			levels = 'Infant';
+		}
+		else if (clicks < 100) {
+			levels = 'Child';
+		}
+		else if (clicks < 200) {
+			levels = 'Teen';
+		}
+		else if (clicks < 500) {
+			levels = 'Adult';
+		}
+		else {
+			levels = 'Ninja';
+		}
+		return levels;
+	}, this);
 }
 
 ko.applyBindings(new ViewModel());
